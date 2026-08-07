419c7c82380335dc1279ca217295a40529606e8f
ffffre377-coder <ffffre377@gmail.com>
2026-08-07 23:59:46 +0530

    Update build.gradle.kts

--- camera-vision/build.gradle.kts
+++ camera-vision/build.gradle.kts
@@ -5,11 +5,19 @@ plugins {
 
 android {
     namespace = "com.samim.jarvis.camera"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
 
     compileOptions {
@@ -26,4 +34,12 @@ dependencies {
     implementation("androidx.camera:camera-core:1.3.0")
     implementation("androidx.camera:camera-camera2:1.3.0")
     implementation("androidx.camera:camera-lifecycle:1.3.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // Compose
+    implementation("androidx.compose.ui:ui:1.4.3")
+    implementation("androidx.compose.material:material:1.4.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
 }
