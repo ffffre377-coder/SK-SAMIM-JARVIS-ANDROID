50f82d94b08411ad37bbd9705221d61d4d78d80d
ffffre377-coder <ffffre377@gmail.com>
2026-08-07 23:14:35 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
